6d28955df791fb70b34971158f0fd4794a702873
John Westhoff <johnjwesthoff@gmail.com>
2018-04-10 02:17:53 -0400

    got some sort of r/place working

--- app.py
+++ app.py
@@ -2,16 +2,66 @@
 import cherrypy
 from cherrypy.lib import static
 import os
+import urllib
+import json
+import threading
+import time
+from datetime import datetime
+
+TIMEOUT = 30
+
+colors={'cell_{}'.format(x): 'rgb(255,255,255)' for x in range(0, 32*32)}
+colors['v'] = 1
+
+output = None
+lock = threading.Lock()
 
 class Page(object):
     @cherrypy.expose
-    def index(self):
-        return "<html><h1>Welcome to my project</h1></html>"
+    def read(self, v):
+        ct = datetime.now()
+        try:
+            lock.acquire()
+            while int(v) >= int(colors['v']):
+                lock.release()
+                time.sleep(0.1)
+                lock.acquire()
+                lt = datetime.now()
+                if int((lt - ct).total_seconds()) >= TIMEOUT:
+                    break
+            return json.dumps(colors)
+        except Exception as E:
+            return json.dumps({'Error': str(E)})
+        finally:
+            lock.release()
+
+    @cherrypy.expose
+    def write(self, id, color):
+        try:
+            lock.acquire()
+            colors[id] = urllib.parse.unquote(color)
+            output.write('{}|{}\n'.format(id, color))
+            output.flush();
+            colors['v'] += 1
+            return 'success'
+        except Exception as E:
+            return str(E)
+        finally:
+            lock.release()
+
 if __name__ == '__main__':
+    with open("storage.txt") as f:
+        for l in f:
+            id, color = l.strip().split('|')
+            colors[id] = color
+    output = open("storage.txt", "a+")
     conf = {
         '/': {
             'tools.sessions.on': True,
-            'tools.staticdir.root': os.path.abspath(os.getcwd())
+            'tools.staticdir.root': os.path.join(os.path.abspath(os.getcwd()), 'static'),
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './',
+            'tools.staticdir.index': 'index.html'
         },
         '/assets': {
             'tools.staticdir.on': True,
